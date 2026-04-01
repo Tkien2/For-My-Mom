6a3bad085b293505b4565306b1bb5bb81f08d25f
Tkien2 <trantrungkien29012009@gmail.com>
2026-04-01 11:26:43 +0700

    tl

--- setup.js
+++ setup.js
@@ -10,19 +10,19 @@ async function playBubble(n){
     let count = 0
     while(true){
         const bubble = document.createElement("div")
-        const bubbleScale = getRandomNum(0.7,1.3)
+        const bubbleScale = getRandomNum(0.5,0.8)
         bubble.classList.add("bubble")
-        bubble.style.left = `${Math.random()*(window.innerWidth - (bubble.offsetWidth*bubbleScale))}px`
         bubble.style.scale = `${bubbleScale}`
         bubble.animationDuration = `${getRandomNum(8,13)}s`
         // bubble.classList.add(`bubbleColor${Math.round(getRandomNum(1,5))}`)
         document.getElementById("bubblesBackground").appendChild(bubble)
+        bubble.style.left = `${getRandomNum(bubble.offsetWidth, window.innerWidth - bubble.offsetWidth)}px`
         if(count >= n){
             document.querySelector(".bubble").remove()
             count--
         }
         count++
-        await wait(getRandomNum(800,2200))
+        await wait(getRandomNum(800,1700))
     }
 }
 playBubble(10)
